cd73842d04d25c601a715fd29df4abee3ca7f446
Thakur Prasad Rout <78676104+thakurtpr@users.noreply.github.com>
2023-05-26 17:06:41 +0530

    Added solution - LeetHub

---
+++ Selection Sort - GFG/selection-sort.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    int select(int arr[], int i)
+    {
+        selectionSort(arr, i);
+    }
+     
+    void selectionSort(int arr[], int n)
+    { 
+        int i,j;
+       for( i= 0;i<=n-2;i++){
+           int min = i;
+           for( j = i; j<=n-1;j++){
+               if(arr[j]<arr[min]){
+                   min = j;
+               }
+           }
+           int temp = arr[min] ;
+           arr[min] = arr[i];
+           arr[i] = temp;
+           
+       }
+    }
+};
+
+//{ Driver Code Starts.
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    Solution ob;  
+    ob.selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
