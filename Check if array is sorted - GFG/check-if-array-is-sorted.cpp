17766eab5d03a9a4e8bc633ebcc9262243dcf48c
Thakur Prasad Rout <78676104+thakurtpr@users.noreply.github.com>
2023-05-31 11:25:53 +0530

    Added solution - LeetHub

---
+++ Check if array is sorted - GFG/check-if-array-is-sorted.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        int v = 0;
+        for(int i = 0 ;i<n-1 ;i++){
+            if (arr[i]<=arr[i+1]){
+                if(i==(n-2)){
+                    v=1;
+                }
+            }
+            else 
+             return false;
+        }
+        if(n==1)
+         return true;
+        if(v==1){
+            return true;
+        }
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        bool ans = ob.arraySortedOrNot(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
